918f1e9cdce238bf8b8d68e3f258855bce092499
Riddhi <143718084+zekely@users.noreply.github.com>
2026-04-01 09:20:58 +0530

    Update iceskating_piano.js
    -Changed the commands to the correct Strudel defined effects

--- iceskating_piano.js
+++ iceskating_piano.js
@@ -1,25 +1,21 @@
 stack(
-  //piano chords (slow + airy)
-  note("<[0,4,7] ~ [2,5,9] ~ [4,7,11] ~ [5,9,12]>")
-    .sound("gm_piano")
-    .scale("major")
-    .root("d")
+  // piano chords (slow + airy)
+  note("<[d4,f#4,a4] [e4,g4,b4] [f#4,a4,c#5] [g4,b4,d5]>")
     .slow(4)
+    .sound("piano")
     .gain(0.6)
-    .room(0.8)
-    .reverb(0.7),
+    .room(0.8),
 
-  //high notes
-  note("12 ~ 14 ~ 16 ~ 19")
-    .sound("gm_piano")
-    .scale("major")
-    .root("d")
+  // high melody notes
+  note("<d5 e5 f#5 a5>")
     .slow(6)
+    .sound("piano")
     .gain(0.3)
     .delay(0.4)
-    .delaytime(0.3),
+    .delaytime(0.3)
+    .delayfeedback(0.3),
 
-  // very light rhythm
+  // light hi-hat rhythm
   s("~ hh ~ ~")
     .gain(0.2)
     .room(0.9)
